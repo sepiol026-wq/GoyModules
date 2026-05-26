6f1d33039b0989cff00f8e79025a4778041748fd
sam sepiol <sepiol026@gmail.com>
2026-05-27 00:45:26 +0300

    bump to 2.2.9

--- vector.py
+++ vector.py
@@ -16,7 +16,7 @@
 # meta banner: https://raw.githubusercontent.com/sepiol026-wq/GoyModules/refs/heads/main/assets/vector.png
 # meta developer: @GoyModules
 
-__version__ = (2, 2, 8)
+__version__ = (2, 2, 9)
 
 import asyncio
 import base64
