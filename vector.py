0636cc8efb83ea22c1e937f44ef72c9b19e63dd9
sam sepiol <sepiol026@gmail.com>
2026-05-28 23:42:39 +0300

    feat: show dependencies in module card (API already returns them)

--- vector.py
+++ vector.py
@@ -1382,7 +1382,27 @@ class Vector(loader.Module):
                             cl.append(f"... +{len(cmds) - len(cl)} more")
                         cmd_block = f"{hdr}{chr(10).join(cl)}{ftr}"
 
-        return f"{'\n'.join(pfx)}{desc_block}{cmd_block}".rstrip()[:CAP]
+        # --- dependencies ---
+        deps = m_data.get("dependencies", [])
+        dep_block = ""
+        if deps:
+            est = used + len(desc_block) + len(cmd_block) + 20
+            if est < CAP - 15:
+                hdr = f"\n\n{self.ICONS['dependency']} <b>{self.strings.get('v_deps', 'Dependencies')}:</b>\n<blockquote expandable>"
+                ftr = "</blockquote>"
+                room = CAP - used - len(desc_block) - len(cmd_block) - len(hdr) - len(ftr) - 3
+                if room > 0:
+                    dl = []
+                    for d in deps:
+                        dt = utils.escape_html(str(d))
+                        if room - len(dt) - 3 < 0:
+                            break
+                        dl.append(f"<code>{dt}</code>")
+                        room -= len(dt) + 3
+                    if dl:
+                        dep_block = f"{hdr}{', '.join(dl)}{ftr}"
+
+        return f"{'\n'.join(pfx)}{desc_block}{cmd_block}{dep_block}".rstrip()[:CAP]
 
     def _build_kbd(self, item: dict, idx: int, group: list, search_phrase: str, is_expanded: bool = False) -> list:
         log.debug("_build_kbd: name=%s idx=%d expanded=%s", item.get("name", "?"), idx, is_expanded)
