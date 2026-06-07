ca2a683cf6248c887bb60641087251967729c078
sam sepiol <sepiol026@gmail.com>
2026-06-07 09:00:47 +0300

    delete bot token response message after extracting JWT

--- vector.py
+++ vector.py
@@ -1337,6 +1337,8 @@ class Vector(loader.Module):
                         if new_jwt: break
                     except asyncio.TimeoutError:
                         with suppress(Exception): await out_msg.delete()
+                    else:
+                        with suppress(Exception): await resp.delete()
             except Exception as e:
                 log.warning("Token conversation attempt=%s failed: %r", attempt, e)
 
