b041b901c402fd01e355dd9543f734cb97e7b8b3
sam sepiol <sepiol026@gmail.com>
2026-06-07 09:41:03 +0300

    revert bot message deletion

--- vector.py
+++ vector.py
@@ -1337,8 +1337,6 @@ class Vector(loader.Module):
                         if new_jwt: break
                     except asyncio.TimeoutError:
                         with suppress(Exception): await out_msg.delete()
-                    else:
-                        with suppress(Exception): await self.client.delete_messages(bot_username, [resp.id], revoke=True)
             except Exception as e:
                 log.warning("Token conversation attempt=%s failed: %r", attempt, e)
 
