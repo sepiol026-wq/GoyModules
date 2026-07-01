555233930e26a4b807776f29beb0acc4fad0b692
Sam Sepi0l <sepiol026@gmail.com>
2026-07-02 07:09:35 +0900

    Rewrite user-facing meta tags for sc

--- sc.py
+++ sc.py
@@ -16,7 +16,7 @@
 
 # requires: telethon pillow yt-dlp imageio-ffmpeg curl_cffi
 # meta developer: @goymodules
-# meta tags: music, soundcloud, downloader, audio, playlist, музыка, саундклауд, загрузчик, аудио, плейлист
+# meta tags: music, soundcloud, download, playlist, search, heroku, музыка, саундклауд, скачивание, плейлист, поиск, хероку
 # authors: @goymodules
 # Description: SoundCloud media module.
 # meta banner: https://raw.githubusercontent.com/sepiol026-wq/goypulse/main/assets/soundcloudmusic.png
