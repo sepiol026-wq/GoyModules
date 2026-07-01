75666b5eb596106b83a10fff38eb3d277eecf811
Sam Sepi0l <sepiol026@gmail.com>
2026-07-02 07:09:17 +0900

    Rewrite user-facing meta tags for RedConstructor

--- RedConstructor.py
+++ RedConstructor.py
@@ -15,7 +15,7 @@
 # ====================================================================================================================
 # meta banner: https://raw.githubusercontent.com/sepiol026-wq/GoyModules/refs/heads/main/assets/RedConstructor.png
 # meta developer: @GoyModules
-# meta tags: ide, module-builder, code-generation, development, heroku, иде, конструктор-модулей, генерация-кода, разработка, хероку
+# meta tags: module-builder, code-generation, constructor, development, heroku, конструктор-модулей, генерация-кода, разработка, хероку
 # requires: aiohttp ast hashlin 
 
 __version__ = ("1", "0", "0")
