e97f5003c7de33009bee73f73c2fbea88da8e8a0
Sam Sepi0l <sepiol026@gmail.com>
2026-07-02 07:05:44 +0900

    Add Russian meta tags for iris

--- iris.py
+++ iris.py
@@ -12,7 +12,7 @@
 
 # requires: html
 # meta developer: @GoyModules
-# meta tags: ai, iris, control, assistant, heroku
+# meta tags: ai, iris, control, assistant, heroku, ии, ирис, управление, ассистент, хероку
 # authors: @goymodules
 # Description: Iris control.
 # meta banner: https://raw.githubusercontent.com/sepiol026-wq/GoyModules/refs/heads/main/assets/iris.png
