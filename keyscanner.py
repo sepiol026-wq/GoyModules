45e6d12583a88d47b5fcb004b4c9cf56b26bb08a
Sam Sepi0l <sepiol026@gmail.com>
2026-07-02 07:01:17 +0900

    Add meta tags for keyscanner

--- keyscanner.py
+++ keyscanner.py
@@ -15,6 +15,7 @@
 # ====================================================================================================================
 # meta banner: https://raw.githubusercontent.com/sepiol026-wq/GoyModules/refs/heads/main/assets/keyscanner.png
 # meta developer: @GoyModules
+# meta tags: security, api-keys, credentials, scanner, recon, heroku
 # requires: aiohttp aiohttp-socks
 
 __version__ = (2, 5, 8)
